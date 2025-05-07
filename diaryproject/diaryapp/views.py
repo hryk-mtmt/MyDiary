9f2bee01ad949c0e9d7da9e02ac9e195d91da996
hryk-mtmt <mtmt2532@gmail.com>
2025-05-07 14:07:53 +0900

    first commit

---
+++ diaryproject/diaryapp/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import DiaryModel
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+ # 継承する汎用 View のインポート
+from django.urls import reverse_lazy
+
+class DiaryList(ListView):
+    template_name = 'diaryapp/list.html'
+    model = DiaryModel
+
+class DiaryCreate(CreateView):
+    template_name = 'diaryapp/create.html'
+    model = DiaryModel
+    fields = ('date','title','text')
+    success_url = reverse_lazy('list')
+
+class DiaryDetail(DetailView):
+    template_name = 'diaryapp/detail.html'
+    model = DiaryModel
+
+class DiaryUpdate(UpdateView):
+    template_name = 'diaryapp/update.html'
+    model = DiaryModel
+    fields = ('date','title','text')
+    success_url = reverse_lazy('list')
+
+class DiaryDelete(DeleteView):
+    template_name = 'diaryapp/delete.html'
+    model = DiaryModel
+    fields = ('date','title','text')
+    success_url = reverse_lazy('list')
